b2ed9d4d345496ff3f732bf2915cbbfcf4185804
Ruth Baumbach <80909370+RBaumbachF@users.noreply.github.com>
2025-01-16 11:06:20 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,18 +1,6 @@
-// create web server with express
-const express = require('express');
-const app = express();
-
-// create a route
-// app.get('/', (req, res) => {
-//   res.send('Hello World');
-// });
-
-// create a route with a parameter
-app.get('/comments/:id', (req, res) => {
-  res.send('Comment ID: ' + req.params.id);
-});
-
-// start the server
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+// Create web server
+// Start web server
+// Create a route
+// Create a route handler
+// Send a response
+// Test your work
